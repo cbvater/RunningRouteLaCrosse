63e96a58dec0921594a40607b5af3608b69ad9c7
Logan Johnson <LGJohnson1305@gmail.com>
2026-03-25 18:29:10 -0500

    added gender and city
    Signed-off-by: Logan Johnson <LGJohnson1305@gmail.com>

--- src/main/java/com/example/demo/model/Runner.java
+++ src/main/java/com/example/demo/model/Runner.java
@@ -13,19 +13,29 @@ public class Runner {
     private Long RunnerId;
 
     //Creates variables
-    private String RunnerName;
-    private float RunnerPR;
-    private double TotalMiles;
-    private int TotalRuns;
-    private String FavoriteRoute;
-    private String FavoriteTerrain;
+    private String Runner_Name;
+    private int age;
+    private int height;
+    private int weight;
+    private String gender;
+    private String city;
+    private float Runner_PR;
+    private double Total_Miles;
+    private int Total_Runs;
+    private String Favorite_Route;
+    private String Favorite_Terrain;
 
     //Creates getter methods
-    public String getRunnerName() { return RunnerName; }
-    public float getRunnerPR() { return RunnerPR; }
-    private double getTotalMiles() { return TotalMiles; }
-    private int getTotalRuns() { return TotalRuns; }
-    private String getFavoriteRoute() { return FavoriteRoute; }
-    private String getFavoriteTerrain() { return FavoriteTerrain; }
+    public String getRunnerName() { return Runner_Name; }
+    public int getAge() { return age; }
+    public int getHeight() { return height; }
+    public int getWeight() { return weight; }
+    public String getGender() { return gender; }
+    public String getCity() { return city; }
+    public float getRunnerPR() { return Runner_PR; }
+    private double getTotalMiles() { return Total_Miles; }
+    private int getTotalRuns() { return Total_Runs; }
+    private String getFavoriteRoute() { return Favorite_Route; }
+    private String getFavoriteTerrain() { return Favorite_Terrain; }
 
 }
