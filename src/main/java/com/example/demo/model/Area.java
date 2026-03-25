fb2a4715c57354d1e9a0255fe13dd6f854fe7c50
CBangs3924 <connordb06@gmail.com>
2026-03-25 11:37:06 -0500

    did stuff to area class
    Signed-off-by: CBangs3924 <connordb06@gmail.com>

--- src/main/java/com/example/demo/model/Area.java
+++ src/main/java/com/example/demo/model/Area.java
@@ -1,4 +1,18 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "area")
 public class Area {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long AreaId;
+    private String description;
+    private String AreaName;
+
+    public String getDescription() { return description; }
+    public String getAreaName() {return AreaName; }
 }
