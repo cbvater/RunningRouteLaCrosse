51f7466ca39e827ff4e2a864fc21897677e5e187
CBangs3924 <connordb06@gmail.com>
2026-04-01 11:22:26 -0500

    started the inarea table
    Signed-off-by: CBangs3924 <connordb06@gmail.com>

--- src/main/java/com/example/demo/model/InArea.java
+++ src/main/java/com/example/demo/model/InArea.java
@@ -1,4 +1,18 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "inArea")
+
 public class InArea {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long areaId;
+    private String areaName;
+
+    public Long getAreaId(){return areaId;}
+    public String getAreaName(){return areaName;}
 }
